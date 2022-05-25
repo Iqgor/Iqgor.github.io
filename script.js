5853de5200ef33f35441986ab898e57184bdedf0
Igor <33993@ma-web.nl>
2022-05-25 10:29:26 +0200

    eerste commit

---
+++ script.js
@@ -0,0 +1,78 @@
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const message = document.getElementById("message");
+const output = document.getElementById("result");
+const image1 = document.getElementById("image1");
+
+startRecognition = () => {
+  if (SpeechRecognition !== undefined) { // test if speechrecognitio is supported
+    let recognition = new SpeechRecognition();
+    recognition.lang = 'nl-NL'; // which language is used?
+    recognition.interimResults = false; // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition/interimResults
+    recognition.continuous = false; // https://developer.mozilla.org/en-US/docs/Web/API/SpeechRecognition/continuous
+   
+    recognition.onstart = () => {
+      message.innerHTML = `Starting listening, speak in the microphone please<br>Say "help me" for help`;
+      output.classList.add("hide"); // hide the output
+    };
+
+    recognition.onspeechend = () => {
+      message.innerHTML = `I stopped listening `;
+      recognition.stop();
+    };
+
+    recognition.onresult = (result) => {
+      let transcript = result.results[0][0].transcript;
+      let confidenceTranscript= Math.floor(result.results[0][0].confidence * 100); // calc. 'confidence'
+      output.classList.remove("hide"); // show the output
+      output.innerHTML = `I'm ${confidenceTranscript}% certain you just said: <b>${transcript}</b>`;
+      actionSpeech(transcript);
+    };
+
+    recognition.start();
+  } 
+  else {  // speechrecognition is not supported
+    message.innerHTML = "sorry speech to text is not supported in this browser";
+  }
+};
+
+// process speech results
+actionSpeech = (speechText) => {
+  speechText = speechText.toLowerCase().trim(); // trim spaces + to lower case
+  console.log(speechText); // debug 
+  switch(speechText){ 
+    // switch evaluates using stric comparison, ===
+    case "igor":
+      window.open("https://www.linkedin.com/in/igor-herrebrugh-10476323a/", "_self");
+      break;
+    case  "reset":
+      document.body.style.background = "#ffe6ab";
+      document.body.style.color="#000000";
+      image1.classList.add("hide"); // hide image (if any)
+      break;
+    case "image": // let op, "fall-through"
+    case "caroline": // let op, "fall-through"
+      image1.src = "./img/caroline.jpg";
+      image1.style.width = "400px";
+      image1.classList.remove("hide") // show image
+      break;
+    case "help me":
+      alert("Valid speech commands: black,  reset, next page");
+      break;
+    case "iphone":
+      window.open("https://www.apple.com/nl/iphone/", "_self");
+      break;
+    case "jeroen":
+      image1.src = "./img/1633781911017.jpg";
+      image1.style.width = "400px";
+      image1.classList.remove("hide")
+      break;
+    case "horloge":
+      image1.src = "img/index.jpg"
+      image1.style.widows = "400px"
+      image1.classList.remove("hide")
+      break;
+    default:
+      // do nothing yet
+  }
+}
